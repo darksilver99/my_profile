65fbc5f6d49ba0a5fddd6ec05cee3ae072d0ae0b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-29 02:35:52 +0000

    Updating to latest FlutterFlow output.

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.silver.myprofile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/silver/myprofile/my_profile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.silver.myprofile.my_profile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
